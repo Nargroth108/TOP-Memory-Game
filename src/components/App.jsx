0d0be920ddbf09e60be856f7010b6f1e13420400
Nargroth108 <138591543+Nargroth108@users.noreply.github.com>
2024-02-25 17:05:14 +0100

    Display fetched images on page in random order

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,15 @@ export default function App() {
   const [artist, setArtist] = useState("Dreamcatcher");
   const [cardData, setCardData] = useState([]);
 
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
   useEffect(() => {
     fetch(
       `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist}&api_key=bbcc545698a179be8fefc4ef44ce2c0d&limit=12&format=json`,
@@ -17,27 +26,36 @@ export default function App() {
       .then((response) => {
         const data = [];
         const albums = response.topalbums.album;
+        let id = 0;
         albums.forEach((album) => {
           data.push({
             name: album.name,
-            url: album.image[1]["#text"],
+            url: album.image[3]["#text"],
             clicked: false,
+            id: id,
           });
+          id += 1;
         });
+
+        shuffleArray(data);
 
         setCardData(data);
       })
       .catch((error) => {
         console.log(error);
       });
+
+    return () => {
+      setCardData([]);
+    };
   }, [artist]);
 
-  console.log(cardData);
+  // console.log(cardData);
 
   return (
     <>
-      <Header setArtist={setArtist} />
-      <CardField />
+      <Header setArtist={setArtist} cardData={cardData} />
+      <CardField key={"cardField"} cardData={cardData} />
     </>
   );
 }
--- src/components/CardField.jsx
+++ src/components/CardField.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable react/prop-types */
 import "../styles/CardField.css";
 
-export default function CardField() {
+export default function CardField({ cardData }) {
   return (
-    <>
-      <section>card</section>
-    </>
+    <div className="cardField" key={"cardField"}>
+      {cardData.map((card) => {
+        return (
+          <section key={card.id} className="card">
+            <img src={card.url} alt={card.name} />
+            <h4 className="cardTitle">{card.name}</h4>
+          </section>
+        );
+      })}
+    </div>
   );
 }
